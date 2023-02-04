0f6b49a7bed6e2a9645d5e7844f013239da1176e
Riley <117216536+SIGMOID27@users.noreply.github.com>
2023-02-03 23:12:56 -0600

    f

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ use std::ops::Range;
 use rand::Rng;
 use sdl2::rect::Point;
 
-const WIDTH: f32 = 400.0;
-const HEIGHT: f32 = 400.0;
+const WIDTH: f32 = 800.0;
+const HEIGHT: f32 = 800.0;
 const FORCES: Range<f32> = -1.0..1.0;
 const RADII: Range<f32> = 20.0..80.0;
 const VISCOSITY: f32 = 0.7;
 const POPULATION: usize = 100;
-const COLORS: [Color; 4] = [
+const VARIANTS: usize = 4;
+const COLORS: [Color; VARIANTS] = [
     Color::RED,
     Color::CYAN,
     Color::GREEN,
@@ -23,7 +24,7 @@ const COLORS: [Color; 4] = [
 
 struct Dots {
     radius: f32,
-    forces: [f32; COLORS.len()],
+    forces: [f32; VARIANTS],
     dots: [Dot; POPULATION],
 }
 
@@ -41,13 +42,13 @@ fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut events = ctx.event_pump().unwrap();
     let mut rng = rand::thread_rng();
-    let mut world: [Dots; COLORS.len()] = COLORS.map(|_| Dots {
+    let mut world: [Dots; VARIANTS] = array::from_fn(|_| Dots {
         radius: rng.gen_range(RADII),
         forces: array::from_fn(|_| rng.gen_range(FORCES)),
         dots: array::from_fn(|_| Dot {
             x: rng.gen_range(0.0..WIDTH),
-            y: 0.0,
-            vx: rng.gen_range(0.0..HEIGHT),
+            y: rng.gen_range(0.0..HEIGHT),
+            vx: 0.0,
             vy: 0.0
         })
     });
@@ -58,18 +59,22 @@ fn main() {
         }
         canvas.set_draw_color(Color::BLACK);
         canvas.clear();
-        for i in 0..COLORS.len() {
-            let world_split = world.split_at_mut(i);
+        for i in 0..VARIANTS {
+            let world_split = world.split_at_mut(i + 1);
             let dots = &mut world_split.0[i];
             let force = dots.forces[i];
-            let points: [Point; POPULATION] = array::from_fn(|j| {
-                let dots_split = dots.dots.split_at_mut(j);
+            let mut points = [Point::new(0, 0); POPULATION];
+            for j in 0..POPULATION {
+                let dots_split = dots.dots.split_at_mut(j + 1);
                 let dot = &mut dots_split.0[j];
                 for other in dots_split.1 {
                     let dx = dot.x - other.x;
                     let dy = dot.y - other.y;
                     let d = dx * dx + dy * dy;
-
+                    if d < dots.radius {
+                        dot.vx += dx / d;
+                        dot.vy += dy / d;
+                    }
                 }
                 for other_dots in world_split.1.iter_mut() {
                     let other_force = other_dots.forces[i];
@@ -80,11 +85,13 @@ fn main() {
                 dot.vx *= VISCOSITY;
                 dot.x += dot.vx;
                 if dot.x < 0.0 || dot.x >= WIDTH { dot.x -= dot.vx * 2.0 }
+
                 dot.vy *= VISCOSITY;
                 dot.y += dot.vy;
                 if dot.y < 0.0 || dot.y >= WIDTH { dot.y -= dot.vy * 2.0 }
-                Point::new(dot.x as i32, dot.y as i32)
-            });
+
+                points[j] = Point::new(dot.x as i32, dot.y as i32);
+            }
             canvas.set_draw_color(COLORS[i]);
             canvas.draw_points(points.as_slice()).unwrap();
         }
